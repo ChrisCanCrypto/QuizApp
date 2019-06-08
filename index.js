c556988364c5f6856844cd47cd45bce4c77fb767
Chris Wysocki <christopher.wysocki@gmail.com>
2019-06-07 19:51:22 -0700

    all functionallity working. Will need to update CSS and make sure that the flow is working in future tests

--- index.js
+++ index.js
@@ -1,1 +1,139 @@
- 
+// Global Vars
+let questionNumber = 0;
+let score = 0;
+let isFinalQuestion = false;
+
+function startQuiz(){
+    $('.start-button').on('click', function(){
+        // console.log("linked and working");
+        $('.quiz-start ').addClass('hidden');
+        $('.question-form').removeClass('hidden');
+        renderQuestion();
+        updateQuesitonNumberText();
+    });
+}
+
+function updateQuesitonNumberText(){
+    $('.question-number').text(questionNumber+1);
+}
+
+function updateQuestionNumber(){
+    questionNumber++;
+    if(questionNumber === QUESTIONS.length-1){
+        isFinalQuestion = true;
+    }
+}
+
+function updateScore(){
+    score++;
+    $('.score').text(score);
+}    
+
+function createQuestion(){
+    let question = QUESTIONS[questionNumber].question;
+    return question;
+}
+
+function createAnswers(){
+    let totalAnswers = QUESTIONS[questionNumber].answers.length;
+    let answersHTML = '<fieldset>'
+    for(let i = 0; i < totalAnswers; i++){
+        answersHTML += '<label class="answerOption"> <input type="radio" value="ans'+i+'" name="answer" required><span>';
+        answersHTML += QUESTIONS[questionNumber].answers[i];
+        answersHTML += '</span></label>'
+    }
+    answersHTML += '<button type="submit" class="submit-button">Submit</button></fieldset>'
+    return answersHTML;
+}
+
+
+function renderQuestion(){
+    $('.question-text').html(createQuestion());
+    $('form').html(createAnswers());
+}
+
+function submitAnswer(){
+    $('form').on('submit', function(event){
+        event.preventDefault();
+        let userAnswer = $('input:checked');
+        let correctAnswer = QUESTIONS[questionNumber].correctAnswer;
+        updateResultInfo();
+        if(userAnswer.val() === correctAnswer){
+            correctResponse();
+        }else{
+            wrongResponse();
+        }
+    })
+}
+
+function updateResultInfo(){
+    $('.response-img').attr('src', QUESTIONS[questionNumber].image);
+    $('.response-img').attr('alt', QUESTIONS[questionNumber].alt);
+    $('.response-flavor').text(QUESTIONS[questionNumber].flavorText);
+}
+
+function updateResultFlavor(){
+
+}
+
+function correctResponse(){
+
+    updateScore();
+    if (isFinalQuestion === true){
+        $('.next-question').addClass('hidden');
+        $('.view-results').removeClass('hidden');
+    }
+    $('.question-form').addClass('hidden');
+    $('.correct-ans').removeClass('hidden');
+    $('.response').removeClass('hidden');
+}
+
+function wrongResponse(){
+
+    if (isFinalQuestion === true){
+        $('.next-question').addClass('hidden');
+        $('.view-results').removeClass('hidden');
+    }
+    $('.question-form').addClass('hidden');
+    $('.wrong-ans').removeClass('hidden');
+    $('.response').removeClass('hidden');
+}
+
+function nextQuesiton(){
+
+    $('.next-question').on('click', function(){
+        $('.question-form').removeClass('hidden');
+        $('.correct-ans').addClass('hidden');
+        $('.wrong-ans').addClass('hidden');
+        $('.response').addClass('hidden');
+        updateQuestionNumber();
+        updateQuesitonNumberText();
+        renderQuestion()
+    });
+
+}
+
+function viewResults(){
+    $('.view-results').on('click', function(){
+        $('.response').addClass('hidden');
+        $('.results').removeClass('hidden');
+        if(score > 3){
+            $('.high-score').removeClass('hidden');
+        }else if(score < 2){
+            $('.low-score').removeClass('hidden');
+        }else{
+            $('.med-score').removeClass('hidden');
+        }
+    });
+}
+
+function createQuiz(){
+    startQuiz();
+    submitAnswer();
+    nextQuesiton();
+    viewResults();
+}
+
+$(createQuiz);
+
+
---
+++ questions.js
@@ -0,0 +1,72 @@
+const QUESTIONS = [
+    {
+        question: 'Of the many goals in the Green New Deal which of the following is NOT inside the resolution?',
+        answers: [
+            'Achieve net-zero greenhouse gas emissions through a fair and just transition for all communities and workers.',
+            'Create millions of good, high-wage jobs; and ensure prosperity and economic security for all people of the United States.',
+            'Overhauling U.S. transportation systems to eliminate pollution and greenhouse gas emissions, by investing in zero-emission vehicle infrastructure and manufacturing, as well as clean, affordable, and accessible public transit and high-speed rail.',
+            'Eliminating all cows from the face of the earth to ensure the elimination of Big Beef in the U.S.' //Correct
+        ],
+        correctAnswer: 'ans3',
+        flavorText: "While cows do contribute to America's carbon footprint there is no effort outlined to eliminate cows and Big Beef in the U.S.",
+        image: 'Img/cows.jpg',
+        alt: 'cows',
+        learnMoreLink: 'https://timeforchange.org/are-cows-cause-of-global-warming-meat-methane-CO2'
+    },
+    {
+        question: 'Which U.S. Senator co-signed the Green New Deal alongside NY Representative Alexandria Ocasio-Cortez?',
+        answers: [
+            'Senator Elizabeth Warren.',
+            'Senator Ed Markey.', //Correct
+            'Senator Kamala Harris.',
+            'Senator Bernie Sanders.'
+        ],
+        correctAnswer: 'ans1',
+        flavorText: "Senator Ed Markey is the co-signer on the Green New Deal. Markey is a senator from Massachuetts and has been involved in public office since 1973.",
+        image: 'Img/SenEd.jpg',
+        alt: 'Senator Ed Markey',
+        learnMoreLink: 'https://en.wikipedia.org/wiki/Ed_Markey'
+    },
+    {
+        question: 'Many grassroots organizations have put their support behind the Green New Deal resolution, which organization is the leader behind the Green New Deal?',
+        answers: [
+            'The Sunrise Movement.', //Correct
+            'The Tea Party.', 
+            'American Civil Liberties Union.',
+            'Democratic National Committee.'
+        ],
+        correctAnswer: 'ans0',
+        flavorText: "The Sunrise Movement is a youth-led movement orginizing around climate change policy for the future of America.",
+        image: 'Img/Sunrise.png',
+        alt: 'Sunrise Movement Logo',
+        learnMoreLink: 'https://www.sunrisemovement.org/'
+    },
+    {
+        question: 'The deadline outlined by the Green New Deal for the U.S. to produce net zero carbon emissions is:',
+        answers: [
+            '2020', 
+            '2050', //Correct
+            '2070',
+            '2100'
+        ],
+        correctAnswer: 'ans1',
+        flavorText: "The 2050 deadline for the United States to produce net-zero carbon emissions aims to curve the projected two degree raising of the Earth's average temprature.",
+        image: 'Img/CleanEnergy.jpg',
+        alt: 'Clean Energy',
+        learnMoreLink: 'https://arstechnica.com/tech-policy/2019/02/green-new-deal-bill-aims-to-move-us-to-100-renewable-energy-net-zero-emissions/'
+    },
+    {
+        question: 'According to the Green New Deal resolution, by the year 2100 how much economic output will the United States lose due to a 2 degree rise in average global temperatures?',
+        answers: [
+            '$1,000,000', 
+            '$50,000,000', 
+            '$100,000,000,000', 
+            '$500,000,000,000' //Correct
+        ],
+        correctAnswer: 'ans3',
+        flavorText: "With a national GDP loss of $500,000,000,000 by 2100 many of the proponants of the Green New Deal believe the aggressive restructuring outlined will pay for itself.",
+        image: 'Img/Money.jpg',
+        alt: 'Money',
+        learnMoreLink: 'https://www.yaleclimateconnections.org/2019/04/climate-change-could-cost-u-s-economy-billions/'
+    },
+]
